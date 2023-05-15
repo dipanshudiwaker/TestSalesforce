d2a95c3f3b39e03c769e8834cae84bff0f0f6302
Dipanshu Diwaker <56470089+dipanshudiwaker@users.noreply.github.com>
2023-05-15 15:07:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,94 +1,63 @@
-import os
-import openai
-import panel as pn 
-from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv()) # read local .env file
+# import libraries
+from flask import Flask, render_template, request
+from newsapi import NewsApiClient
 
-openai.api_key  = os.getenv('OPENAI_API_KEY')
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0, # this is the degree of randomness of the model's output
-    )
-    return response.choices[0].message["content"]
+# init flask app
+app = Flask(__name__)
 
-def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=temperature, # this is the degree of randomness of the model's output
-    )
-#     print(str(response.choices[0].message))
-    return response.choices[0].message["content"]
-def collect_messages(_):
-    prompt = inp.value_input
-    inp.value = ''
-    context.append({'role':'user', 'content':f"{prompt}"})
-    response = get_completion_from_messages(context) 
-    context.append({'role':'assistant', 'content':f"{response}"})
-    panels.append(
-        pn.Row('User:', pn.pane.Markdown(prompt, width=600)))
-    panels.append(
-        pn.Row('Assistant:', pn.pane.Markdown(response, width=600, style={'background-color': '#F6F6F6'})))
- 
-    return pn.Column(*panels)
- # GUI
-pn.extension()
+# Init news api
+newsapi = NewsApiClient(api_key='70fdb9ba81ba40b6bda148e672898bd9')
 
-panels = [] # collect display 
+# helper function
+def get_sources_and_domains():
+	all_sources = newsapi.get_sources()['sources']
+	sources = []
+	domains = []
+	for e in all_sources:
+		id = e['id']
+		domain = e['url'].replace("http://", "")
+		domain = domain.replace("https://", "")
+		domain = domain.replace("www.", "")
+		slash = domain.find('/')
+		if slash != -1:
+			domain = domain[:slash]
+		sources.append(id)
+		domains.append(domain)
+	sources = ", ".join(sources)
+	domains = ", ".join(domains)
+	return sources, domains
 
-context = [ {'role':'system', 'content':"""
-You are OrderBot, an automated service to collect orders for a pizza restaurant. \
-You first greet the customer, then collects the order, \
-and then asks if it's a pickup or delivery. \
-You wait to collect the entire order, then summarize it and check for a final \
-time if the customer wants to add anything else. \
-If it's a delivery, you ask for an address. \
-Finally you collect the payment.\
-Make sure to clarify all options, extras and sizes to uniquely \
-identify the item from the menu.\
-You respond in a short, very conversational friendly style. \
-The menu includes \
-pepperoni pizza  12.95, 10.00, 7.00 \
-cheese pizza   10.95, 9.25, 6.50 \
-eggplant pizza   11.95, 9.75, 6.75 \
-fries 4.50, 3.50 \
-greek salad 7.25 \
-Toppings: \
-extra cheese 2.00, \
-mushrooms 1.50 \
-sausage 3.00 \
-canadian bacon 3.50 \
-AI sauce 1.50 \
-peppers 1.00 \
-Drinks: \
-coke 3.00, 2.00, 1.00 \
-sprite 3.00, 2.00, 1.00 \
-bottled water 5.00 \
-"""} ]  # accumulate messages
+@app.route("/", methods=['GET', 'POST'])
+def home():
+	if request.method == "POST":
+		sources, domains = get_sources_and_domains()
+		keyword = request.form["keyword"]
+		related_news = newsapi.get_everything(q=keyword,
+									sources=sources,
+									domains=domains,
+									language='en',
+									sort_by='relevancy')
+		no_of_articles = related_news['totalResults']
+		if no_of_articles > 100:
+			no_of_articles = 100
+		all_articles = newsapi.get_everything(q=keyword,
+									sources=sources,
+									domains=domains,
+									language='en',
+									sort_by='relevancy',
+									page_size = no_of_articles)['articles']
+		return render_template("home.html", all_articles = all_articles,
+							keyword=keyword)
+	else:
+		top_headlines = newsapi.get_top_headlines(country="in", language="en")
+		total_results = top_headlines['totalResults']
+		if total_results > 100:
+			total_results = 100
+		all_headlines = newsapi.get_top_headlines(country="in",
+													language="en",
+													page_size=total_results)['articles']
+		return render_template("home.html", all_headlines = all_headlines)
+	return render_template("home.html")
 
-
-inp = pn.widgets.TextInput(value="Hi", placeholder='Enter text here…')
-button_conversation = pn.widgets.Button(name="Chat!")
-
-interactive_conversation = pn.bind(collect_messages, button_conversation)
-
-dashboard = pn.Column(
-    inp,
-    pn.Row(button_conversation),
-    pn.panel(interactive_conversation, loading_indicator=True, height=300),
-)
-
-dashboard
-
-messages =  context.copy()
-messages.append(
-{'role':'system', 'content':'create a json summary of the previous food order. Itemize the price for each item\
- The fields should be 1) pizza, include size 2) list of toppings 3) list of drinks, include size   4) list of sides include size  5)total price '},    
-)
- #The fields should be 1) pizza, price 2) list of toppings 3) list of drinks, include size include price  4) list of sides include size include price, 5)total price '},    
-
-response = get_completion_from_messages(messages, temperature=0)
-print(response)
+if __name__ == "__main__":
+	app.run(debug = True)
